fb8c55aa82d699fe8ca07d131c6da722a1f2987e
kamlesh123123 <121994522+kamlesh123123@users.noreply.github.com>
2024-07-12 08:40:00 +0000

    dataypes

---
+++ basics/variables.js
@@ -0,0 +1,22 @@
+const accountId = 145245
+let accountEmail = "kamlesh@google.com"
+var accountPassword = "12456"
+accountCity = "jaipur"
+let accountState ;
+// accountId = 5   //not allowed
+
+accountEmail = "kaml@gmail"
+accountPassword = 7854
+accountCity = "kota"
+
+
+// first way 
+console.log(accountId)
+console.log(accountEmail)
+console.log(accountPassword)
+console.log(accountCity)
+console.log(accountState)
+
+// second way 
+
+console.table ([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ datatypes.js
@@ -0,0 +1,9 @@
+/*
+number = 255
+bigint = 457892220552
+string = "kamlesh"
+boolean = true/false 
+null = standalone value
+undefined = 
+symbol = unique 
+*/
