1760a830d94de6cc3cda64cb804ebcfcf23482ce
kamlesh123123 <121994522+kamlesh123123@users.noreply.github.com>
2024-07-15 08:12:53 +0000

    about string

--- basics/datatypes_summery.js
+++ basics/datatypes_summery.js
@@ -15,7 +15,7 @@
     const id = Symbol('123')
     const anotherId = Symbol('123')
 
-    console.log(id === anotherId);
+    // console.log(id === anotherId);
 
 // refrence (Non primitive )
 
@@ -23,17 +23,46 @@
 
 // array
 const heros = ["shaktiman", "naagraj", "doga"]
-console.log(typeof heros);
+// console.log(typeof heros);
 
 // objects
 let myObj = {
     name : "kamlesh",
     age : 45,
 }
-console.log(typeof myObj);
+// console.log(typeof myObj);
 
 // function
 const myFunction = function() {
-    console.log("Hello World");
+    // console.log("Hello World");
 }
-console.log(typeof myFunction);
+// console.log(typeof myFunction);
+
+
+// ++++++++++++++++++++++++++++++++++++++++
+
+//  Memories
+
+// stack (primitive) , Heap(non-primitive)
+
+
+let myYoutube = "kmaleshkumawat"
+
+let anothername = myYoutube
+
+anothername = "hiii hello"
+
+console.log(myYoutube);
+console.log(anothername);
+
+let user1 = {
+    email : "user@email",
+    upi : "user@ybl",
+}
+
+let user2 = user1
+
+user2.email = "kamlesh@mail" 
+
+console.log(user1.email);
+console.log(user2.email);
