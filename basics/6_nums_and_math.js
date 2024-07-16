a8cf09fd7cb2c2731431b562aec2425d894598ee
kamlesh123123 <121994522+kamlesh123123@users.noreply.github.com>
2024-07-16 06:22:39 +0000

    date and time

---
+++ basics/6_nums_and_math.js
@@ -0,0 +1,35 @@
+const score = 500
+// console.log(score);
+const balance = new Number(100)
+
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 123.57
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-in'));
+
+// ++++++++++++++++++++++++++++++++++
+
+//  MATH
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(3.2));
+// console.log(Math.floor(7.2));
+// console.log(Math.min(7,2,5,8,1,6));
+// console.log(Math.max(7,2,5,8,1,6));
+
+console.log(Math.random());
+console.log((Math.random()*10)+5);
+
+const min = 10
+const max = 10
+
+console.log(Math.floor(Math.random() * (max - min +1) ) + min);
